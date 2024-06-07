0620910d109aa2a321c2d5dfb8c2d9994bb9958a
Mummal <142950902+Mummal@users.noreply.github.com>
2024-06-07 13:28:17 +0800

    Add files via upload

---
+++ HAPPYBLOG-admin/src/main/java/com/HAPPYBLOG/BlogAdminApplication.java
@@ -0,0 +1,12 @@
+package com.HAPPYBLOG;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+@MapperScan("com.sangeng.mapper")
+public class BlogAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAdminApplication.class, args);
+    }
+}
